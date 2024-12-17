14bac76c6dc321d271b1829c2d0307d6b8178cd5
Youssef97dev <youssefmoussafi97@gmail.com>
2024-12-17 16:46:31 +0100

    chnge background

--- src/app/reservation/page.jsx
+++ src/app/reservation/page.jsx
@@ -3,7 +3,7 @@ import ReservationForm from "./components/ReservationForm";
 
 const page = () => {
   return (
-    <div className="bg-[url('https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/Purehouse/t1vpjfjqiortlyd5bnzr')] bg-cover bg-center bg-no-repeat w-full h-[120vh] px-4 lg:px-20 xl:px-80 py-14 lg:py-10 flex flex-col justify-start items-center gap-5">
+    <div className="bg-[url('https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/Purehouse/wdxmef9qfurvrlsodkh1')] bg-cover bg-center bg-no-repeat w-full h-[120vh] px-4 lg:px-20 xl:px-80 py-14 lg:py-10 flex flex-col justify-start items-center gap-5">
       <Logo />
       <ReservationForm />
     </div>
